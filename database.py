018309b389fa1b591330b94b1081f33ae172759d
Liu Jie <maxazure@gmail.com>
2023-11-13 00:03:09 +1300

    add config

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://notes:37100456@192.168.31.205:3306/notes"
+from config import SQLALCHEMY_DATABASE_URL
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL
